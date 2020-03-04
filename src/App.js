0f0febc66f744c1b277c511e284d23859c2a912f
conbec <56614504+conbec@users.noreply.github.com>
2020-03-04 14:50:00 -0800

    read and displsy working for both chefs and recipes

--- src/App.js
+++ src/App.js
@@ -2,15 +2,24 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 
-const Chefs = ({ chefs }) => {
-  console.log(chefs);
+const Chefs = ({ chefs, recipes }) => {
+
   return (
     <div id="chef-container">
       <h2>chefs</h2>
       <ul>
-        {chefs.map(cook => {
+        {chefs.map(chef => {
         return (
-          <li key={cook.id}>{cook.name}</li>
+          <li key={chef.id}>
+            {chef.name}
+            <ul>
+              {recipes.filter(dish => dish.chefId === chef.id).map(dish => {
+                return (
+                  <li key={dish.id}>{dish.name}</li>
+                )
+              })}
+            </ul>
+          </li>
         )
       })}
       </ul>
@@ -19,18 +28,51 @@ const Chefs = ({ chefs }) => {
 }
 
 
+const Recipes = ({ chefs, recipes }) => {
+  return (
+    <div id="recipe-container">
+      <h2>Recipes</h2>
+      <ul>
+        {recipes.map(dish => {
+          return (
+            <li key={dish.id}>
+              {dish.name}
+              <ul>
+                {chefs.filter(cook => cook.id === dish.chefId).map(chef => {
+                  return (
+                    <li key={chef.id}>{chef.name}</li>
+                  )
+                })}
+              </ul>
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
+}
+
+
 const App = () => {
   const [chefs, setChefs] = useState([]);
+  const [recipes, setRecipes] = useState([]);
 
   useEffect(() => {
-    axios.get('/api/chefs')
-      .then(response => setChefs(response.data));
+    Promise.all([
+      axios.get('/api/chefs'),
+      axios.get('/api/recipes')
+    ])
+      .then(values => {
+        setChefs(values[0].data);
+        setRecipes(values[1].data);
+      })
   }, []);
 
   return (
     <div>
       <h1>Acme Bakery</h1>
-      <Chefs chefs={chefs} />
+      <Chefs chefs={chefs} recipes={recipes} />
+      <Recipes chefs={chefs} recipes={recipes} />
     </div>
   )
 }
